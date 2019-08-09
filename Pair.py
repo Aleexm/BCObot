def74bdd07480b8bbc03e624d5188ba443386b99
Alex Mandersloot <alexmandersloot@hotmail.nl>
2019-08-09 03:03:09 +0200

    commentary

--- Pair.py
+++ Pair.py
@@ -31,28 +31,29 @@ class Pair:
 
     # TODO: This can't really be in Pair as it's impossible to detect opposing "can't be moved" for example
     def executeActions(self, action_name, players, active_player):
-        processed_cards = {}
+        processed_cards = {} # dict containing cards whose actions we've executed
         while True:
-            added = False
+            added = False # Boolean to check whether any actions remain
             card_options = []
-            for action in action_name:
+            for action in action_name: # action is a function object, i.e. chrono.close
                 if action not in processed_cards.keys():
                     options_to_add = action(players, active_player, self)
-                    if options_to_add is not None:
+                    if options_to_add is not None: # This function has a playerchoice
                         added = True
                         card_options.append(options_to_add)
-            if not added:
+            if not added: # Nothing to be executed
                 break
-            options = {}
+            options = {} # Dictionary for pretty user printing
             i = 1
             for option_list in card_options:
                 for option in option_list:
                     options[i] = option
                     i+=1
-            if len(options) == 1:
+            if len(options) == 1: # NO choice to be made
                 chosen_option = 1
             else:
                 chosen_option = players[active_player].strategy.chooseOption(options)
+            # options[chosen_option] contains [function object, name of card, user text, integer related to choice]
             action = options[chosen_option][0]
             action(players, active_player, self, chosen_option=options[chosen_option][3])
             processed_cards[action] = True
